7b82b764984b40ce3a87256da07a9775dcee673b
Sidney Roberto <sidneyrpsilva@gmail.com>
2023-02-21 11:06:26 -0300

    📦 refactor: extract contry data for  a new file

--- src/Components/Carrousel/Carrousel.tsx
+++ src/Components/Carrousel/Carrousel.tsx
@@ -5,52 +5,8 @@ import { Box } from '@chakra-ui/react';
 import { A11y, Navigation, Pagination } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
+import { countryData } from '../../data/country';
 import { CarrouselItem } from './CarrouselItem';
-
-interface CountryData {
-  bgImg: string;
-  title: string;
-  description: string;
-}
-
-const countryData: CountryData[] = [
-  {
-    bgImg:
-      'https://images.unsplash.com/photo-1522083165195-3424ed129620?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&q=80',
-    title: 'América do Norte',
-    description: 'Desenvolvido, multicultural e influente na economia global.',
-  },
-  {
-    bgImg:
-      'https://images.unsplash.com/photo-1619546952812-520e98064a52?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&q=80',
-    title: 'América do Sul',
-    description: 'Rico em cultura, recursos naturais e diversidade geográfica.',
-  },
-  {
-    bgImg:
-      'https://images.unsplash.com/photo-1493780474015-ba834fd0ce2f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&q=80',
-    title: 'Ásia',
-    description: 'Maior continente, rico em diversidade cultural e geográfica.',
-  },
-  {
-    bgImg:
-      'https://images.unsplash.com/photo-1547471080-7cc2caa01a7e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&q=80',
-    title: 'África',
-    description: 'Berço da humanidade, rica em diversidade cultural e natural.',
-  },
-  {
-    bgImg:
-      'https://images.unsplash.com/photo-1519677100203-a0e668c92439?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&q=80',
-    title: 'Europa',
-    description: 'Rica em história, cultura e diversidade geográfica.',
-  },
-  {
-    bgImg:
-      'https://images.unsplash.com/photo-1506973035872-a4ec16b8e8d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&q=80',
-    title: 'Oceania',
-    description: 'Cenário de beleza natural e cultura diversa.',
-  },
-];
 
 export function Carrousel() {
   return (
@@ -66,13 +22,9 @@ export function Carrousel() {
         onSwiper={(swiper) => console.log(swiper)}
         onSlideChange={() => console.log('slide change')}
       >
-        {countryData.map(({ bgImg, description, title }) => (
+        {countryData.map(({ bgImg, subtitle, title }) => (
           <SwiperSlide key={title}>
-            <CarrouselItem
-              bgImg={bgImg}
-              title={title}
-              description={description}
-            />
+            <CarrouselItem bgImg={bgImg} title={title} description={subtitle} />
           </SwiperSlide>
         ))}
       </Swiper>
---
+++ src/data/country.ts
@@ -0,0 +1,71 @@
+interface CountryData {
+  bgImg: string;
+  title: string;
+  subtitle: string;
+  description: string;
+  countrySize: number;
+  languagesSize: number;
+}
+
+export const countryData: CountryData[] = [
+  {
+    bgImg:
+      'https://images.unsplash.com/photo-1522083165195-3424ed129620?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&q=80',
+    title: 'América do Norte',
+    subtitle: 'Desenvolvido, multicultural e influente na economia global.',
+    description:
+      'A América do Norte é um continente conhecido por sua diversidade cultural e natural, oferecendo aos turistas paisagens deslumbrantes, cidades modernas, uma rica gastronomia, bem como atrações históricas e culturais, incluindo museus e locais de patrimônio.',
+    countrySize: 3,
+    languagesSize: 256,
+  },
+  {
+    bgImg:
+      'https://images.unsplash.com/photo-1619546952812-520e98064a52?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&q=80',
+    title: 'América do Sul',
+    subtitle: 'Rico em cultura, recursos naturais e diversidade geográfica.',
+    description:
+      'A América do Sul é um continente de grande riqueza cultural e natural, oferecendo aos turistas uma variedade de atrações, desde praias exóticas e florestas tropicais até cidades históricas e sítios arqueológicos. A região é conhecida por sua música vibrante, gastronomia única e hospitalidade calorosa.',
+    countrySize: 12,
+    languagesSize: 456,
+  },
+  {
+    bgImg:
+      'https://images.unsplash.com/photo-1493780474015-ba834fd0ce2f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&q=80',
+    title: 'Ásia',
+    subtitle: 'Maior continente, rico em diversidade cultural e geográfica.',
+    description:
+      'A Ásia é o maior e mais diverso continente do mundo, oferecendo aos turistas uma experiência única. Com sua rica história, cultura e tradições, a Ásia oferece uma infinidade de destinos turísticos fascinantes, incluindo templos budistas, praias de areia branca, montanhas majestosas e uma rica gastronomia.',
+    countrySize: 50,
+    languagesSize: 2300,
+  },
+  {
+    bgImg:
+      'https://images.unsplash.com/photo-1547471080-7cc2caa01a7e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&q=80',
+    title: 'África',
+    subtitle: 'Berço da humanidade, rica em diversidade cultural e natural.',
+    description:
+      'A África é um continente de beleza natural sem igual, com uma rica diversidade de vida selvagem, paisagens, culturas e tradições. Os turistas podem explorar savanas vastas, desertos fascinantes, praias de areia branca e aprender sobre a história e a cultura dos povos locais em locais históricos e museus.',
+    countrySize: 54,
+    languagesSize: 2000,
+  },
+  {
+    bgImg:
+      'https://images.unsplash.com/photo-1519677100203-a0e668c92439?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&q=80',
+    title: 'Europa',
+    subtitle: 'Rica em história, cultura e diversidade geográfica.',
+    description:
+      'A Europa é um continente com uma rica história e cultura, oferecendo aos turistas uma variedade de atrações, desde cidades vibrantes até vilarejos pitorescos. A região é conhecida por sua arte, arquitetura, música, gastronomia e vida noturna.',
+    countrySize: 50,
+    languagesSize: 24,
+  },
+  {
+    bgImg:
+      'https://images.unsplash.com/photo-1506973035872-a4ec16b8e8d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&q=80',
+    title: 'Oceania',
+    subtitle: 'Cenário de beleza natural e cultura diversa.',
+    description:
+      ' A Oceania é um continente de beleza natural surpreendente, oferecendo aos turistas praias de areia branca, águas cristalinas, paisagens exóticas e cidades modernas. A região é conhecida por sua cultura diversa, incluindo a rica cultura aborígene da Austrália e a cultura polinésia das ilhas do Pacífico.',
+    countrySize: 14,
+    languagesSize: 1470,
+  },
+];
