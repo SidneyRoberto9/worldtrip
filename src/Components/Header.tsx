a60709b4c2497721dd8f619feb00d4d47d522bf4
Sidney Roberto <sidneyrpsilva@gmail.com>
2023-02-21 14:17:35 -0300

    📦 refactor: Navigation to home in header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,9 +1,12 @@
 import { Flex, Image } from '@chakra-ui/react';
+import Link from 'next/link';
 
 export function Header() {
   return (
     <Flex w={'100vw'} h={100} align={'center'} justify={'center'}>
-      <Image src="/logo.svg" alt="logo" />
+      <Link href={'/'}>
+        <Image src="/logo.svg" alt="logo" />
+      </Link>
     </Flex>
   );
 }
